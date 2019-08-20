6e2ccb38862e3b64845bbd8c00834628ebb6d8fe
陈金伟 <Chen_JinWell@163.com>
2019-08-20 14:42:29 +0800

    init

---
+++ js/source/base.js
@@ -0,0 +1,18 @@
+
+//POST请求
+function ajaxPost(url, data, success, error, dataType) {
+    var jqxhr = $.post(url, data, function (data, textStatus, xhr) {
+            if (typeof success === 'function')
+                success(data, textStatus, xhr);
+        }, dataType)
+        .error(function (xhr) {
+            if (typeof error === 'function')
+                error(xhr);
+        });
+    return jqxhr;
+}
+
+//GET请求
+function ajaxGet(){
+    $.get();
+}
---
+++ js/source/template2.js
@@ -0,0 +1,109 @@
+//绑定左侧相关基本信息
+function bindLeftData(data,pat){ 
+    new Vue({
+        el:"#leftBaseInfo",
+        data:{
+            items:data
+        }
+    }); 
+
+    new Vue({
+        el:"#pat",
+        data:{
+            Name:"欧阳小路",
+            Sex:"女"
+        }
+    }); 
+}
+ 
+//得分统计
+function gradeCharts(){
+ 
+   var myChart = echarts.init($("#echart_gradeRecord")[0]);
+ 
+   var option = {
+       title: {
+           text: '得分记录'
+       },
+       tooltip: {},
+       legend: {
+           data:['销量']
+       },
+       xAxis: { 
+           name: 'Y/CM',
+           data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+       },
+       color : ['#208dc7'],
+       calculable: true,
+       yAxis: {
+        name: 'Y/CM'
+       },
+       series: [{
+           name: '销量',
+           type: 'bar',
+           data: [5, 20, 36, 10, 10, 20]
+       }]
+   };
+ 
+   myChart.setOption(option); 
+}
+
+
+//运动轨迹
+function sportCharts(){
+ 
+    var myChart = echarts.init($("#echart_sportRecord")[0]);
+  
+    var option = {
+        title: {
+            text: '滚动轨迹'
+        },
+        tooltip: {},
+        legend: {
+            data:['销量']
+        },
+        xAxis: {
+            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        color : ['#208dc7'],
+        calculable: true,
+        yAxis: {},
+        series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
+  
+    myChart.setOption(option); 
+ }
+
+
+ //运动轨迹
+function percentageofSports(){
+ 
+    var myChart = echarts.init($("#echart_Percentageofsports")[0]);
+  
+    var option = {
+        title: {
+            text: '运动占比'
+        },
+        tooltip: {},
+        legend: {
+            data:['销量']
+        },
+        xAxis: {
+            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        color : ['#208dc7'],
+        calculable: true,
+        yAxis: {},
+        series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
+  
+    myChart.setOption(option); 
+ }
